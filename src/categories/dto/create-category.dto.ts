ab39d83cc350326052620fa6dd0714248e9c626f
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2025-04-08 00:54:14 -0600

    Add validation to CreateCategoryDto and log request body in CategoriesController

--- src/categories/dto/create-category.dto.ts
+++ src/categories/dto/create-category.dto.ts
@@ -1,1 +1,7 @@
-export class CreateCategoryDto {}
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateCategoryDto {
+  @IsString({message: 'El campo de la categoría debe ser un string'})
+  @IsNotEmpty({message: 'El campo de la categoría no puede estar vacío'})
+  name: string
+}
